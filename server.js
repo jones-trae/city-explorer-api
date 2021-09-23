b169ecb47bc0b315995ce4146eaf2d777f9cf809
jones-trae <jones.trae@gmail.com>
2021-09-23 09:48:36 -0700

    test

--- server.js
+++ server.js
@@ -12,4 +12,11 @@ const app = express();
 
 app.use(cors());
 
+
+//routes
+app.get('/', request, response => console.log('any string'));
+
+
+
+//listener
 app.listen(PORT, () => console.log('Listening on PORT' + PORT));
